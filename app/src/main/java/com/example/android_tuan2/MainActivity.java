3225a52204253fe9d3a26929beb260fc78d8dd03
Admin <phamlethanh2021@gmail.com>
2022-12-16 03:07:11 +0700

    Initial commit

---
+++ app/src/main/java/com/example/android_tuan2/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.android_tuan2;
+
+import static com.example.android_tuan2.R.layout.bai2_b;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(bai2_b);
+    }
+}
